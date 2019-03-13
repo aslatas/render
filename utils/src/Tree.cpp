b31c8f29ebf8eaaf939663f46af116dd77833c0b
drhollar <drhollar>
2019-03-13 14:04:31 -0400

    added documentation to the quadtree

--- utils/src/Tree.cpp
+++ utils/src/Tree.cpp
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <assert.h>
 #define STB_DS_IMPLEMENTATION
@@ -13,64 +11,72 @@
 #define OCT_TREE_CHILDREN 8
 #define OCT_TREE_BIN_SIZE 20
 
-static bool helper_add(QuadTree *qt, int position, Model* model);
-Node* CreateNode(float* s_min, float* s_max, size_t per_element_bin_size, Node* parent);
+// Helper Functions for the QuadTree
+static bool  helper_add(QuadTree *qt, int position, Model *model);
+static void  helper_print_quad_tree(Node **tree, int position);
+static void  helper_free_quad_tree(Node **tree, int position);
+static void  Split(QuadTree *qt, int position);
+static Node* CreateNode(float *s_min, float *s_max, size_t per_element_bin_size, Node *parent);
 
-// TODO(Dustin): Change to bit shifting for better efficiency
+
 static bool
-CheckAxisPointOverlapp(float min_x, float max_x, float min_y, float max_y) {
-  return (min_x <= min_y && min_y <= max_x && max_x >= max_y && max_y >= min_x) || // X--y------y--X
-         (min_x >= min_y && min_y <= max_x && max_x >= max_y && max_y >= min_x) || // y--X------y--X
-         (min_x <= min_y && min_y <= max_x && max_x <= max_y && max_y >= min_x) || // X--y------X--y
-         (min_x >= min_y && min_y <= max_x && max_x <= max_y && max_y >= min_x);   // y--X------X--y
+CheckAxisPointOverlapp(float min_x, float max_x, float min_y, float max_y)
+{
+    return (min_x <= min_y && min_y <= max_x && max_x >= max_y && max_y >= min_x) || // X--y------y--X
+           (min_x >= min_y && min_y <= max_x && max_x >= max_y && max_y >= min_x) || // y--X------y--X
+           (min_x <= min_y && min_y <= max_x && max_x <= max_y && max_y >= min_x) || // X--y------X--y
+           (min_x >= min_y && min_y <= max_x && max_x <= max_y && max_y >= min_x);   // y--X------X--y
 }
 
-bool CheckBoundingBoxCollision2D(AABB_2D& boxA, AABB_2D& boxB) {
+bool CheckBoundingBoxCollision2D(AABB_2D &boxA, AABB_2D &boxB)
+{
     return CheckAxisPointOverlapp(boxA.min[0], boxA.max[0], boxB.min[0], boxB.max[0]) &&
            CheckAxisPointOverlapp(boxA.min[1], boxA.max[1], boxB.min[1], boxB.max[1]);
 }
 
-bool CheckBoundingBoxCollision3D(AABB_3D& boxA, AABB_3D& boxB) {
-  return CheckAxisPointOverlapp(boxA.min[0], boxA.max[0], boxB.min[0], boxB.max[0]) &&
-         CheckAxisPointOverlapp(boxA.min[1], boxA.max[1], boxB.min[1], boxB.max[1]) &&
-         CheckAxisPointOverlapp(boxA.min[2], boxA.max[2], boxB.min[2], boxB.max[2]);
+bool CheckBoundingBoxCollision3D(AABB_3D &boxA, AABB_3D &boxB)
+{
+    return CheckAxisPointOverlapp(boxA.min[0], boxA.max[0], boxB.min[0], boxB.max[0]) &&
+           CheckAxisPointOverlapp(boxA.min[1], boxA.max[1], boxB.min[1], boxB.max[1]) &&
+           CheckAxisPointOverlapp(boxA.min[2], boxA.max[2], boxB.min[2], boxB.max[2]);
 }
 
-static void 
-Split(QuadTree* qt, int position) {
-    Node* node = qt->tree[position];
+static void
+Split(QuadTree *qt, int position)
+{
+    Node *node = qt->tree[position];
 
-    AABB_2D* aabb = node->bounding_box;
-    float* min = aabb->min;
-    float* max = aabb->max;
+    AABB_2D *aabb = node->bounding_box;
+    float *min = aabb->min;
+    float *max = aabb->max;
 
     float tl_min[2] = {0, max[1] / 2};
-    float tl_max[2] = {max[0] / 2, max[1]}; 
+    float tl_max[2] = {max[0] / 2, max[1]};
 
     float tr_min[2] = {max[0] / 2, max[1] / 2};
-    float tr_max[2] = {max[0], max[1]}; 
+    float tr_max[2] = {max[0], max[1]};
 
     float bl_min[2] = {0, 0};
-    float bl_max[2] = {max[0] / 2, max[1] / 2}; 
+    float bl_max[2] = {max[0] / 2, max[1] / 2};
 
     float br_min[2] = {max[0] / 2, 0};
     float br_max[2] = {max[0], max[1] / 2};
 
-    if ( (position * QUAD_TREE_CHILDREN) + 4 >= arrlen(qt->tree))
+    if ((position * QUAD_TREE_CHILDREN) + 4 >= arrlen(qt->tree))
     {
         arrsetlen(qt->tree, (arrlen(qt->tree) + 1) * 4);
     }
 
-    qt->tree[(position * QUAD_TREE_CHILDREN) + 1] = CreateNode(tl_min, tl_max, sizeof(int), node); 
-    qt->tree[(position * QUAD_TREE_CHILDREN) + 2] = CreateNode(tr_min, tr_max, sizeof(int), node); 
-    qt->tree[(position * QUAD_TREE_CHILDREN) + 3] = CreateNode(bl_min, bl_max, sizeof(int), node); 
+    qt->tree[(position * QUAD_TREE_CHILDREN) + 1] = CreateNode(tl_min, tl_max, sizeof(int), node);
+    qt->tree[(position * QUAD_TREE_CHILDREN) + 2] = CreateNode(tr_min, tr_max, sizeof(int), node);
+    qt->tree[(position * QUAD_TREE_CHILDREN) + 3] = CreateNode(bl_min, bl_max, sizeof(int), node);
     qt->tree[(position * QUAD_TREE_CHILDREN) + 4] = CreateNode(br_min, br_max, sizeof(int), node);
 
     node->isLeaf = false;
 
-    for (int i = 0; i < node->bin->count; ++i) 
+    for (int i = 0; i < node->bin->count; ++i)
     {
-        Model* model = node->bin->model[i];
+        Model *model = node->bin->model[i];
         helper_add(qt, position, model);
     }
 
@@ -81,9 +87,10 @@ Split(QuadTree* qt, int position) {
 }
 
 static bool
-helper_add(QuadTree* qt, int position, Model* model) {
+helper_add(QuadTree *qt, int position, Model *model)
+{
 
-    Node* node = qt->tree[position];
+    Node *node = qt->tree[position];
     if (!node->isLeaf) // this node has children, need to go further down the tree
     {
         bool at_least_one_node_intersection_found = false;
@@ -115,12 +122,11 @@ helper_add(QuadTree* qt, int position, Model* model) {
                 assert((position * QUAD_TREE_CHILDREN) + (i + 1) < arrlen(qt->tree)); // the child node should be within bounds of the tree
                 if (CheckBoundingBoxCollision2D(*qt->tree[(position * QUAD_TREE_CHILDREN) + i]->bounding_box, model->aabb))
                 {
-                    int p = (position * QUAD_TREE_CHILDREN) + (i + 1);
-                    helper_add(qt, p, model);
+                    helper_add(qt, (position * QUAD_TREE_CHILDREN) + (i + 1), model);
                 }
             }
         }
-        else 
+        else
         {
             arrput(node->bin->model, model);
             ++node->bin->count;
@@ -130,64 +136,66 @@ helper_add(QuadTree* qt, int position, Model* model) {
     }
 }
 
-bool Add(QuadTree* qt, Model* model) {
+bool Add(QuadTree *qt, Model *model)
+{
 
     // Preliminary check to verify the model exists within bounds of the tree
-    if (!CheckBoundingBoxCollision2D(*qt->tree[0]->bounding_box, model->aabb)) 
-        return false;    
+    if (!CheckBoundingBoxCollision2D(*qt->tree[0]->bounding_box, model->aabb))
+        return false;
 
     return helper_add(qt, 0, model);
 }
 
-static Node* 
-CreateNode(float* s_min, float* s_max, size_t per_element_bin_size, Node* parent) {
+static Node*
+CreateNode(float *s_min, float *s_max, size_t per_element_bin_size, Node *parent)
+{
+    size_t node_size = sizeof(Node);
 
-  size_t node_size = sizeof(Node);
+    AABB_2D *aabb = (AABB_2D *)malloc(sizeof(AABB_2D));
+    ;
+    aabb->min[0] = s_min[0];
+    aabb->min[1] = s_min[1];
+    aabb->max[0] = s_max[0];
+    aabb->max[1] = s_max[1];
 
-  AABB_2D* aabb = (AABB_2D*)malloc(sizeof(AABB_2D));;
-  aabb->min[0] = s_min[0];
-  aabb->min[1] = s_min[1];
-  aabb->max[0] = s_max[0];
-  aabb->max[1] = s_max[1];
+    Node *n = (Node *)malloc(sizeof(Node));
+    n->bounding_box = aabb;
+    n->parent = parent;
+    n->isLeaf = true;
+    n->bin = (Bin *)malloc(sizeof(Bin));
+    n->bin->count = 0;
+    n->bin->model = nullptr;
 
-  Node *n = (Node*)malloc(sizeof(Node));
-  n->bounding_box = aabb;
-  n->parent       = parent;
-  n->isLeaf       = true;
-  n->bin          = (Bin*)malloc(sizeof(Bin));
-  n->bin->count   = 0;
-  n->bin->model   = nullptr;
-
-  return n;
+    return n;
 }
 
 // Creates a Quad tree given a min and max bounds
 // min and max should be a 2 element array
-QuadTree* CreateQuadTree(float* min, float* max, size_t element_size_bin) 
+QuadTree *CreateQuadTree(float *min, float *max, size_t element_size_bin)
 {
-  QuadTree* qt = (QuadTree*)malloc(sizeof(QuadTree));
-  qt->num_levels = 0;
-  qt->size_element_per_bin = element_size_bin;
-  qt->tree = nullptr;
+    QuadTree *qt = (QuadTree *)malloc(sizeof(QuadTree));
+    qt->num_levels = 0;
+    qt->size_element_per_bin = element_size_bin;
+    qt->tree = nullptr;
 
-  arrsetlen(qt->tree, 1);
-  qt->tree[0] = CreateNode(min, max, element_size_bin, nullptr);
+    arrsetlen(qt->tree, 1);
+    qt->tree[0] = CreateNode(min, max, element_size_bin, nullptr);
 
-  return qt;
+    return qt;
 }
 
 static void
-helper_print_quad_tree(Node** tree, int position) 
+helper_print_quad_tree(Node **tree, int position)
 {
     Node *node = tree[position];
 
     printf("Node %d at level %d.\n", position, position / 4);
     printf("This node has this bounding box.\n");
     printf("  Bounding Box:\n      minimum: (%f, %f)\n      maximum: (%f, %f)\n", node->bounding_box->min[0], node->bounding_box->min[1], node->bounding_box->max[0], node->bounding_box->max[1]);
-    
+
     if (!node->parent)
     {
-      printf("  This node is the root.\n");
+        printf("  This node is the root.\n");
     }
     if (!node->isLeaf)
     {
@@ -202,38 +210,38 @@ helper_print_quad_tree(Node** tree, int position)
         printf("  This node has no children.\n");
     }
 
-    if (!node->bin) 
+    if (!node->bin)
     {
         printf("  This node does not have a bin.\n");
     }
-    else 
+    else
     {
-      if (node->bin->count == 0)
-      {
-        printf("  This node has a bin, but contains no models.\n");
-      }
-      else 
-      {
-          printf("  This node's bin contains %d models:\n", node->bin->count);
-        for (int j = 0; j < node->bin->count; ++j)
+        if (node->bin->count == 0)
         {
-            Model* m = node->bin->model[j];
-            printf("    Bounding Box:\n      minimum: (%f, %f)\n      maximum: (%f, %f)\n", m->aabb.min[0], m->aabb.min[1], m->aabb.max[0], m->aabb.max[1]);
-            printf("    Data: %d\n", m->val);
+            printf("  This node has a bin, but contains no models.\n");
         }
-        //printf("  ");
-      }
+        else
+        {
+            printf("  This node's bin contains %d models:\n", node->bin->count);
+            for (int j = 0; j < node->bin->count; ++j)
+            {
+                Model *m = node->bin->model[j];
+                printf("    Bounding Box:\n      minimum: (%f, %f)\n      maximum: (%f, %f)\n", m->aabb.min[0], m->aabb.min[1], m->aabb.max[0], m->aabb.max[1]);
+                printf("    Data: %d\n", m->val);
+            }
+            //printf("  ");
+        }
     }
     printf("\n");
 }
 
-void PrintQuadTree(QuadTree& qt) 
+void PrintQuadTree(QuadTree &qt)
 {
-  helper_print_quad_tree(qt.tree, 0);
+    helper_print_quad_tree(qt.tree, 0);
 }
 
 static void
-helper_free_quad_tree(Node** tree, int position)
+helper_free_quad_tree(Node **tree, int position)
 {
     if (!tree[position]->isLeaf)
     {
@@ -243,7 +251,7 @@ helper_free_quad_tree(Node** tree, int position)
         }
     }
     else
-    {       
+    {
         arrfree(tree[position]->bin->model);
     }
 
@@ -251,7 +259,7 @@ helper_free_quad_tree(Node** tree, int position)
     free(tree[position]);
 }
 
-void FreeQuadTree(QuadTree* qt)
+void FreeQuadTree(QuadTree *qt)
 {
     helper_free_quad_tree(qt->tree, 0);
     arrfree(qt->tree);
@@ -260,7 +268,6 @@ void FreeQuadTree(QuadTree* qt)
 
 void TestBoundingBoxCollisions()
 {
-
     AABB_2D boundsA;
     boundsA.min[0] = 0;
     boundsA.min[1] = 0;
@@ -268,22 +275,22 @@ void TestBoundingBoxCollisions()
     boundsA.max[1] = 10;
 
     AABB_2D boundsB; // tl
-	boundsB.min[0] = 0;
-	boundsB.min[1] = 5;
-	boundsB.max[0] = 5;
-	boundsB.max[1] = 10;
+    boundsB.min[0] = 0;
+    boundsB.min[1] = 5;
+    boundsB.max[0] = 5;
+    boundsB.max[1] = 10;
 
     AABB_2D boundsC; // tr
-	boundsC.min[0] = 5;
-	boundsC.min[1] = 5;
-	boundsC.max[0] = 10;
-	boundsC.max[1] = 10;
+    boundsC.min[0] = 5;
+    boundsC.min[1] = 5;
+    boundsC.max[0] = 10;
+    boundsC.max[1] = 10;
 
     AABB_2D boundsD; // bl
-	boundsD.min[0] = 0;
-	boundsD.min[1] = 0;
-	boundsD.max[0] = 5;
-	boundsD.max[1] = 5;
+    boundsD.min[0] = 0;
+    boundsD.min[1] = 0;
+    boundsD.max[0] = 5;
+    boundsD.max[1] = 5;
 
     AABB_2D boundsE; // br
     boundsE.min[0] = 5;
@@ -301,19 +308,19 @@ void TestBoundingBoxCollisions()
     bbA.min[1] = 0;
     bbA.max[0] = 7;
     bbA.max[1] = 1;
-    assert(CheckBoundingBoxCollision2D(boundsA, bbA)); // full bb
+    assert(CheckBoundingBoxCollision2D(boundsA, bbA));  // full bb
     assert(!CheckBoundingBoxCollision2D(boundsB, bbA)); // tl
     assert(!CheckBoundingBoxCollision2D(boundsC, bbA)); // tr
     assert(!CheckBoundingBoxCollision2D(boundsD, bbA)); // bl
-    assert(CheckBoundingBoxCollision2D(boundsE, bbA)); // br <- FAILS
+    assert(CheckBoundingBoxCollision2D(boundsE, bbA));  // br <- FAILS
 
     AABB_2D bbB; // tl
     bbB.min[0] = 0;
     bbB.min[1] = 6;
     bbB.max[0] = 1;
     bbB.max[1] = 7;
-    assert(CheckBoundingBoxCollision2D(boundsA, bbB)); // full bb
-    assert(CheckBoundingBoxCollision2D(boundsB, bbB)); // tl
+    assert(CheckBoundingBoxCollision2D(boundsA, bbB));  // full bb
+    assert(CheckBoundingBoxCollision2D(boundsB, bbB));  // tl
     assert(!CheckBoundingBoxCollision2D(boundsC, bbB)); // tr
     assert(!CheckBoundingBoxCollision2D(boundsD, bbB)); // bl
     assert(!CheckBoundingBoxCollision2D(boundsE, bbB)); // br
@@ -323,98 +330,83 @@ void TestBoundingBoxCollisions()
     bbC.min[1] = 6;
     bbC.max[0] = 7;
     bbC.max[1] = 7;
-    assert(CheckBoundingBoxCollision2D(boundsA, bbC)); // full bb
+    assert(CheckBoundingBoxCollision2D(boundsA, bbC));  // full bb
     assert(!CheckBoundingBoxCollision2D(boundsB, bbC)); // tl
-    assert(CheckBoundingBoxCollision2D(boundsC, bbC)); // tr
+    assert(CheckBoundingBoxCollision2D(boundsC, bbC));  // tr
     assert(!CheckBoundingBoxCollision2D(boundsD, bbC)); // bl
     assert(!CheckBoundingBoxCollision2D(boundsE, bbC)); // br
-
-    AABB_2D bbD;
-    bbC.min[0] = 4;
-    bbC.min[1] = 4;
-    bbC.max[0] = 6;
-    bbC.max[1] = 6;
-
-    // AABB_2D bbE;
-
-
-    // AABB_2D bbF;
-
-
 }
 
 // Entry point
-int main(void) {
-
+int main(void)
+{
     TestBoundingBoxCollisions();
 
-  float min[2] = {0, 0};
-  float max[2] = {10, 10};
+    float min[2] = {0, 0};
+    float max[2] = {10, 10};
 
-  // printf("MIN BEFORE: %f %f\nMAX BEFORE: %f %f\n", min[0], min[1], max[0], max[1]);
+    QuadTree *qt = CreateQuadTree(min, max, sizeof(int));
 
-  QuadTree* qt = CreateQuadTree(min, max, sizeof(int));
+    Model modelA;
+    modelA.aabb.min[0] = 0;
+    modelA.aabb.min[1] = 0;
+    modelA.aabb.max[0] = 1;
+    modelA.aabb.max[1] = 1;
+    modelA.val = 10;
 
-  Model modelA;
-  modelA.aabb.min[0] = 0;
-  modelA.aabb.min[1] = 0;
-  modelA.aabb.max[0] = 1;
-  modelA.aabb.max[1] = 1;
-  modelA.val = 10;
+    Model modelB;
+    modelB.aabb.min[0] = 11;
+    modelB.aabb.min[1] = 11;
+    modelB.aabb.max[0] = 12;
+    modelB.aabb.max[1] = 12;
+    modelB.val = 20;
 
-  Model modelB;
-  modelB.aabb.min[0] = 11;
-  modelB.aabb.min[1] = 11;
-  modelB.aabb.max[0] = 12;
-  modelB.aabb.max[1] = 12;
-  modelB.val = 20;
+    Model modelC;
+    modelC.aabb.min[0] = 6;
+    modelC.aabb.min[1] = 0;
+    modelC.aabb.max[0] = 7;
+    modelC.aabb.max[1] = 1;
+    modelC.val = 30;
 
-  Model modelC;
-  modelC.aabb.min[0] = 6;
-  modelC.aabb.min[1] = 0;
-  modelC.aabb.max[0] = 7;
-  modelC.aabb.max[1] = 1;
-  modelC.val = 30;
+    Model modelD;
+    modelD.aabb.min[0] = 0;
+    modelD.aabb.min[1] = 6;
+    modelD.aabb.max[0] = 1;
+    modelD.aabb.max[1] = 7;
+    modelD.val = 40;
 
-  Model modelD;
-  modelD.aabb.min[0] = 0;
-  modelD.aabb.min[1] = 6;
-  modelD.aabb.max[0] = 1;
-  modelD.aabb.max[1] = 7;
-  modelD.val = 40;
+    Model modelE;
+    modelE.aabb.min[0] = 6;
+    modelE.aabb.min[1] = 6;
+    modelE.aabb.max[0] = 7;
+    modelE.aabb.max[1] = 7;
+    modelE.val = 50;
 
-  Model modelE;
-  modelE.aabb.min[0] = 6;
-  modelE.aabb.min[1] = 6;
-  modelE.aabb.max[0] = 7;
-  modelE.aabb.max[1] = 7;
-  modelE.val = 50;
+    Model modelF;
+    modelF.aabb.min[0] = 4;
+    modelF.aabb.min[1] = 4;
+    modelF.aabb.max[0] = 6;
+    modelF.aabb.max[1] = 6;
+    modelF.val = 60;
 
-  Model modelF;
-  modelF.aabb.min[0] = 4;
-  modelF.aabb.min[1] = 4;
-  modelF.aabb.max[0] = 6;
-  modelF.aabb.max[1] = 6;
-  modelF.val = 60;
+    assert(Add(qt, &modelA));
+    assert(!Add(qt, &modelB));
+    assert(Add(qt, &modelC));
+    assert(Add(qt, &modelD));
+    assert(Add(qt, &modelE));
 
-  assert(Add(qt, &modelA));
-  assert(!Add(qt, &modelB));
-  assert(Add(qt, &modelC));
-  assert(Add(qt, &modelD));
-  assert(Add(qt, &modelE));
-  
-  // First recursive split
-  assert(Add(qt, &modelF));
+    // First recursive split
+    assert(Add(qt, &modelF));
 
-  // Second recursive split test
-  assert(Add(qt, &modelA));
-  assert(Add(qt, &modelA));
-  assert(Add(qt, &modelA));
-  assert(Add(qt, &modelA)); 
+    // Second recursive split test
+    assert(Add(qt, &modelA));
+    assert(Add(qt, &modelA));
+    assert(Add(qt, &modelA));
+    assert(Add(qt, &modelA));
 
-  PrintQuadTree(*qt);
+    PrintQuadTree(*qt);
 
-  FreeQuadTree(qt);
+    FreeQuadTree(qt);
 
-  return(0);
+    return (0);
 }
--- utils/src/Tree.h
+++ utils/src/Tree.h
@@ -4,50 +4,90 @@
 #include <stdint.h>
 
 // Bounding region for 2D space
+//   min: minimum x,y values located in the model
+//   max: maximum x,y values located in the model
 struct AABB_2D {
   float min[2];
   float max[2];
 };
 // Bounding region for 3D space
+//   min: minimum x,y,z values located in the model
+//   max: maximum x,y,z values located in the model
 struct AABB_3D {
   float min[3];
   float max[3];
 };
 
+// Temporary model used for the Quad tree. Values are currently harcoded
+// into the tree, but this will change in later iterations.
 struct Model {
   // General representation of a model in a scene
   AABB_2D aabb;
   int val; // For now a model is simply an integer type
 };
 
-// TODO(Dustin): In the future this will probably change to a differnet data structure
-// so that we can 
+// Bins contain a list of models.
+//   model: an array of models utilizing the header only library, stb_ds.h.
+//   count: number of models located in the bin.
 struct Bin {
   Model** model= NULL; // list of models in this bin
   uint8_t count = 0;
 };
 
+// A node in the QuadTree.
+//  bounding_box: a 2D bounding box that represents the spatial position of the Node. 
+//  parent: TODO(Dustin): implement. Not currently being used. The type will be changed to int and
+//    will represent the position of the parent in the list. Alternatively, the parent can be found
+//    by (given the position of this node): ((position - 1) / QUAD_TREE_CHILDREN).
+//  Bin: the bin containing a list of models located at this node. The bin will either be allocated or
+//    be set to NULL. When the bin is allocated, the Node will be a leaf node, otherwise the node is a
+//    branch node. At Node creation, the bin is always allocated and assumbed to be a leaf.
+//  isLeaf: a quick check whether or not the Node is a leaf. True if leaf node, false otherwise.
 struct Node {
-  Node *parent   = NULL;
-  Bin  *bin      = NULL;
-  //Node *children = NULL;
-
   // Bounding Box for this node
   AABB_2D* bounding_box;
+
+  Node *parent   = NULL;
+  Bin  *bin      = NULL;
   bool isLeaf = true;
 };
 
+// Quad Tree structure
+//  size_element_per_bin: size of the struct/elements that go into the Bins. This will eventually allow for the bins to be
+//    abstracted from the data  structure, leaving it up to the user to decide how the data interacts with the client.   
+//    TODO(Dustin): currently hard-coded. Fix that.
+//  num_levels: not currently used, but could be useful for restriciting the maximum number of levels the quad tree can have.
+//  tree: an array containing all of the nodes in the tree. If given a position, you can find the location first child Node
+//    by: (postion * QUAD_TREE_CHILDREN) + 1. The array is handled by the header only library, stb_ds.h.
+// Unimplemented:
+//   Render Function: this will be a void* that user sets to control how the tree gets rendered.
+//   Hueristic Functions: Hueristics can be applied to a Tree to improve space/time complexities. The currrent plan is to have
+//     a set of heuristics the user can define should they desire. These hueristics will either be void* for the user to set from
+//     either a custom implementation or one of ours. 
 struct QuadTree {
   size_t size_element_per_bin; // keep? Helps to determine the required space for a bin element
   uint8_t num_levels = 0; // depth of the tree, default is 0
   Node** tree = NULL; // array representation of the tree
 };
 
+// Check bounding box-bounding box collisions. Return true if there was a collision, false otherwise 
 bool CheckBoundingBoxCollision2D(AABB_2D& boxA, AABB_2D& boxB);
 bool CheckBoundingBoxCollision3D(AABB_3D& boxA, AABB_3D& boxB);
 
+// Constructs a quad tree given a minimum and maximum 2D coordinates and the size each model will be.
+//   min: minimum x,y of the quad tree.
+//   max: maximum x,y of the quad tree.
+//   element_size_bin: size of each model that is placed in the bin. Currently not in use.
 QuadTree* CreateQuadTree(float* min, float* max, size_t element_size_bin);
 
+// Add a Model to the QuadTree.
+//   qt: quad tree to add the model to.
+//   model: model to add.
+//   Returns true if the Add operation was successful. False otherwise.
 bool Add(QuadTree* qt, Model* model);
 
+// Prints the quad tree passed as the parameter.
+void PrintQuadTree(QuadTree &qt);
+
+// Free the quad tree passed as the parameter.
 void FreeQuadTree(QuadTree* qt);
